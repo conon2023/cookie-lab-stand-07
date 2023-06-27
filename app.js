5f5866eac26da59657c6c735cd3822886302d60e
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-27 14:19:29 +0100

    updated the header and footer rows with the store totals and hourly totals

---
+++ app.js
@@ -0,0 +1,112 @@
+"use strict";
+console.log("I hope you work buddy");
+
+const container = document.getElementById("container");
+const storeTable = document.getElementById("storeTable");
+
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
+
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Constructor function for CookieLabStand
+function CookieLabStand(
+  storeName,
+  minCustPerHour,
+  maxCustPerHour,
+  avgCookiesPerHour
+) {
+  this.storeName = storeName;
+  this.minCustPerHour = minCustPerHour;
+  this.maxCustPerHour = maxCustPerHour;
+  this.avgCookiesPerHour = avgCookiesPerHour;
+  this.customersEachHour = [];
+  this.cookiesEachHour = [];
+  this.totalDailyCookies = 0;
+  this.calcCustomersEachHour();
+  this.calcCookiesEachHour();
+  this.render();
+}
+
+// Prototype methods for CookieLabStand
+CookieLabStand.prototype.calcCustomersEachHour = function () {
+  for (let i = 0; i < hours.length; i++) {
+    this.customersEachHour.push(
+      randomNum(this.minCustPerHour, this.maxCustPerHour)
+    );
+  }
+};
+
+CookieLabStand.prototype.calcCookiesEachHour = function () {
+  
+  for (let i = 0; i < hours.length; i++) {
+    const oneHour = Math.ceil(
+      this.customersEachHour[i] * this.avgCookiesPerHour
+    );
+    this.cookiesEachHour.push(oneHour);
+    this.totalDailyCookies += oneHour;
+  }
+};
+
+CookieLabStand.prototype.render = function () {
+  // const article = document.createElement("article");
+  // container.appendChild(article);
+
+  // const h3 = document.createElement("h3");
+  // h3.textContent = this.storeName; // look at this later i.e storename
+  // article.appendChild(h3);
+
+  // const table = document.createElement("table");
+  // storeTable.appendChild(table);
+
+  const tr = document.createElement("tr");
+
+  const th = document.createElement("th");
+  th.textContent = this.storeName;
+  tr.appendChild(th);
+
+  // // create a th to display the totals and append to the tr
+  // const storeTotal = document.createElement("th");
+  // storeTotal.textContent = this.totalDailyCookies;
+  // tr.appendChild(storeTotal);
+  console.log(this.cookiesEachHour)
+  // loop through cookiesEachHour - create a td for each index and append to tr
+  for (let i = 0; i < hours.length; i++) {
+    const td = document.createElement("td");
+    td.textContent = this.cookiesEachHour[i];
+    tr.appendChild(td);
+  }
+
+  // create a th to display the totals and append to the tr
+  const storeTotal = document.createElement("th");
+  storeTotal.textContent = this.totalDailyCookies;
+  tr.appendChild(storeTotal);
+
+  // append the tr to the table - storeTable
+  storeTable.appendChild(tr);
+};
+
+// test constructor works
+const seattle = new CookieLabStand("seattle", 23, 65, 6.3);
+const dubai = new CookieLabStand("dubai", 3, 24, 1.2);
+const paris = new CookieLabStand("paris", 11, 38, 3.7);
+const lima = new CookieLabStand("lima", 20, 38, 2.3);
+const tokyo = new CookieLabStand("tokyo", 2, 16, 4.6);
+const totals = new CookieLabStand("totals", 59, 181, 18.1);
+console.log(seattle);
--- sales.js
+++ sales.js
@@ -1,113 +1,157 @@
 "use strict";
-console.log("I hope you work buddy");
+
+const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", ];
 
 const container = document.getElementById("container");
 const storeTable = document.getElementById("storeTable");
+const newStoreForm = document.getElementById("newStoreForm");
 
-const hours = [
-  "6am",
-  "7am",
-  "8am",
-  "9am",
-  "10am",
-  "11am",
-  "12pm",
-  "1pm",
-  "2pm",
-  "3pm",
-  "4pm",
-  "5pm",
-  "6pm",
-  "7pm",
-];
+// empty array to push new stores into when created
+const allStores = [];
 
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
-// Constructor function for CookieLabStand
-function CookieLabStand(
-  storeName,
-  minCustPerHour,
-  maxCustPerHour,
-  avgCookiesPerHour
-) {
-  this.storeName = storeName;
+
+// function randomNum(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+
+function randomNum(min, max){
+  const x = Math.floor(Math.random()*(max - min + 1) + min);
+  return x;
+};
+
+function CookieLabStand(name,minCustPerHour,maxCustPerHour,avgCookiesPerCust){
+  this.name= name;
   this.minCustPerHour = minCustPerHour;
-  this.maxCustPerHour = maxCustPerHour;
-  this.avgCookiesPerHour = avgCookiesPerHour;
+  this.maxCustPerHour= maxCustPerHour;
+  this.avgCookiesPerCust= avgCookiesPerCust;
   this.customersEachHour = [];
   this.cookiesEachHour = [];
   this.totalDailyCookies = 0;
-  this.calcCustomersEachHour();
-  this.calcCookiesEachHour();
+  this.calculateCustomerEachHour();
+  this.calculateCookiesEachHour();
+  this.pushStore();
   this.render();
-}
-
-// Prototype methods for CookieLabStand
-CookieLabStand.prototype.calcCustomersEachHour = function () {
-  for (let i = 0; i < hours.length; i++) {
-    this.customersEachHour.push(
-      randomNum(this.minCustPerHour, this.maxCustPerHour)
-    );
-  }
-};
-
-CookieLabStand.prototype.calcCookiesEachHour = function () {
   
-  for (let i = 0; i < hours.length; i++) {
-    const oneHour = Math.ceil(
-      this.customersEachHour[i] * this.avgCookiesPerHour
-    );
-    this.cookiesEachHour.push(oneHour);
-    this.totalDailyCookies += oneHour;
+};
+
+// Prototype Method for Populating CustomerEachHourArray
+CookieLabStand.prototype.calculateCustomerEachHour = function(){
+  for(let i = 0; i < hours.length; i++){
+  const y = randomNum(this.minCustPerHour, this.maxCustPerHour);
+  this.customersEachHour.push(y);
+  // console.log(i) this is to calculatecustomereachhour`)
+}
+};
+
+CookieLabStand.prototype.calculateCookiesEachHour = function(){
+  for(let i =0;i< hours.length; i++){
+      const z = Math.ceil(this.avgCookiesPerCust * this.customersEachHour[i]);
+      this.cookiesEachHour.push(z)
+      this.totalDailyCookies += z;
   }
 };
 
-CookieLabStand.prototype.render = function () {
-  // const article = document.createElement("article");
-  // container.appendChild(article);
 
-  // const h3 = document.createElement("h3");
-  // h3.textContent = this.storeName; // look at this later i.e storename
-  // article.appendChild(h3);
+//Protopye method for pushing a new intance of CookieLabStand into the all stores array as it is created.//
 
-  // const table = document.createElement("table");
-  // storeTable.appendChild(table);
+CookieLabStand.prototype.pushStore = function(){
+  allStores.push(this);
+  // console.log(allStores)
+};
 
+
+// prototype method used to render new instance of a store into the table
+// steps
+// invokes the calcCustomersEachHour / calcCookiesEachHour methods
+// creates the table row and first table header cell with content
+// create a for loop that loops through for every hour, creates a table data cell and sets the content to the cookies sold for that hour, appends each table data cell to the table row
+// create a table header cell to display totalDailyCookies append to table row
+// append the table row to the table
+
+  CookieLabStand.prototype.render = function () {
+    this.calculateCustomerEachHour();
+    this.calculateCookiesEachHour();
+  
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    th.textContent = this.name;
+    tr.appendChild(th);
+  
+    for (let i = 0; i < hours.length; i++) {
+      const td = document.createElement("td");
+      td.textContent = this.cookiesEachHour[i];
+      tr.appendChild(td);
+    }
+  
+    const storeTotal = document.createElement("th");
+    storeTotal.textContent = this.totalDailyCookies;
+    tr.appendChild(storeTotal);
+  
+    storeTable.appendChild(tr);
+  }
+
+// create a function to render the hour row at the top of the table
+// create the table row and table header cells
+// loop through hours, create a table header cell, set content to hours[i] append to the table row
+// create a table header, set the content to store totals, append to table row, append table row to storeTable
+//invoke the hours row
+
+function hoursRow() {
   const tr = document.createElement("tr");
-  storeTable.appendChild(tr);
-
   const th = document.createElement("th");
-  th.textContent = this.storeName;
+  th.textContent = "Store Location";
   tr.appendChild(th);
 
-  // // create a th to display the totals and append to the tr
-  // const storeTotal = document.createElement("th");
-  // storeTotal.textContent = this.totalDailyCookies;
-  // tr.appendChild(storeTotal);
-  console.log(this.cookiesEachHour)
-  // loop through cookiesEachHour - create a td for each index and append to tr
   for (let i = 0; i < hours.length; i++) {
-    const td = document.createElement("td");
-    td.textContent = this.cookiesEachHour[i];
-    tr.appendChild(td);
+    const th = document.createElement("th");
+    th.textContent = hours[i];
+    tr.appendChild(th);
   }
 
-  // create a th to display the totals and append to the tr
-  const storeTotal = document.createElement("th");
-  storeTotal.textContent = this.totalDailyCookies;
-  tr.appendChild(storeTotal);
-
-  // append the tr to the table - storeTable
+  const storeTotalsHeaderCell = document.createElement("th");
+  storeTotalsHeaderCell.textContent = "Store Totals";
+  tr.appendChild(storeTotalsHeaderCell);
   storeTable.appendChild(tr);
-};
+}
 
-// test constructor works
-const seattle = new CookieLabStand("seattle", 23, 65, 6.3);
-const dubai = new CookieLabStand("dubai", 3, 24, 1.2);
-const paris = new CookieLabStand("paris", 11, 38, 3.7);
-const lima = new CookieLabStand("lima", 20, 38, 2.3);
-const tokyo = new CookieLabStand("tokyo", 2, 16, 4.6);
-const totals = new CookieLabStand("totals", 59, 181, 18.1);
-console.log(seattle);
+  hoursRow()
+
+  //Creates a New Instance of CookieLabStand
+
+  const seattle = new CookieLabStand("Seattle",23,65,6.3);
+  const dubai = new CookieLabStand("dubai", 3, 24, 1.2);
+  const paris = new CookieLabStand("paris", 11, 38, 3.7);
+  const lima = new CookieLabStand("lima", 20, 38, 2.3);
+  const tokyo = new CookieLabStand("tokyo", 2, 16, 4.6);
+
+
+  //  Creates the hourlyTotals 
+  function hourlyTotals() {
+  const tr = document.createElement("tr")
+  const th = document.createElement("th");
+  th.textContent = "Hourly Totals";
+  tr.appendChild(th);
+
+
+   for (let i = 0; i < hours.length; i++) {
+      const th = document.createElement("th");
+      let hoursAdded = 0;
+      for (let j = 0; j < allStores.length; j++) {
+      const hourAmount = allStores[j].cookiesEachHour[i];
+      hoursAdded += hourAmount;
+      }
+      th.textContent = hoursAdded;
+      tr.appendChild(th);
+  }
+  
+  let totalTotals = 0;
+  for (let i = 0; i < allStores.length; i++) {
+      totalTotals += allStores[i].totalDailyCookies;
+  }
+  const totalsCell = document.createElement("th");
+  totalsCell.textContent = totalTotals;
+  tr.appendChild(totalsCell);
+
+  storeTable.appendChild(tr);
+}
+hourlyTotals();
